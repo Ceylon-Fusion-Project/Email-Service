ed3635df0391154e714459eff8155d6588498956
RaviprabhaSanduniJayalal <jayalal12351@usci.ruh.ac.lk>
2025-02-10 10:08:39 +0530

    emailservice,emailtemplate modified

--- src/main/java/com/Email_Service/Email_Service/entity/EmailTemplate.java
+++ src/main/java/com/Email_Service/Email_Service/entity/EmailTemplate.java
@@ -1,16 +1,14 @@
 package com.Email_Service.Email_Service.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
+@Getter
+@Setter
 @Builder
 @Table(name = "email_templates")
 public class EmailTemplate {
@@ -26,4 +24,44 @@ public class EmailTemplate {
     private String body; // Email body with placeholders like {{username}}
 
     private String templateType; // "HTML" or "TEXT"
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTemplateName() {
+        return templateName;
+    }
+
+    public void setTemplateName(String templateName) {
+        this.templateName = templateName;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getTemplateType() {
+        return templateType;
+    }
+
+    public void setTemplateType(String templateType) {
+        this.templateType = templateType;
+    }
 }
